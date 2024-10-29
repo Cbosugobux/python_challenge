6ced30c9012155243f10c0a1814329cc330ab8c8
Chris Bushelman <chrisbushelman@gmail.com>
2024-10-29 15:13:55 -0400

    Update Main.py

--- pypoll/Main.py
+++ pypoll/Main.py
@@ -3,8 +3,8 @@ import csv
 import os
 
 # Files to load and output (update with correct file paths)
-file_to_load = os.path.join("/workspaces","python_challenge", "pypoll", "Resources", "election_data.csv")  # Input file path
-file_to_output = os.path.join("/workspaces", "python_challenge","pypoll", "Analysis", "election_analysis.txt")  # Output file path
+file_to_load = os.path.join("Resources", "election_data.csv")  # Input file path
+file_to_output = os.path.join("Analysis", "election_analysis.txt")  # Output file path
 
 # Initialize variables to track the election data
 total_votes = 0  # Track the total number of votes cast
