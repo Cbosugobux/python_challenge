1dedc715d05a20278d16445cc2dfef7af8c57f8d
Chris Bushelman <chrisbushelman@gmail.com>
2024-10-29 15:13:16 -0400

    Update Main.py
    update paths

--- pybank/Main.py
+++ pybank/Main.py
@@ -2,8 +2,8 @@ import csv
 import os
 
 # Files to load and output (update with correct file paths)
-file_to_load = os.path.join("/workspaces", "python_challenge", "pybank", "Resources", "budget_data.csv")  # Input file path
-file_to_output = os.path.join("/workspaces", "python_challenge", "pybank", "Analysis", "budget_analysis.txt")  # Output file path
+file_to_load = os.path.join("Resources", "budget_data.csv")  # Input file path
+file_to_output = os.path.join("Analysis", "budget_analysis.txt")  # Output file path
 
 # Define variables to track the financial data
 total_months = 0
